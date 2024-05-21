7ece81fa285bbdd60d27c7bf74c75bcb22767071
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-05-21 07:47:01 -0600

    sesion_1705

--- sesion1705/fuciones.cpp
+++ sesion1705/fuciones.cpp
@@ -3,64 +3,121 @@
 
 using namespace std;
 
-/*prototipo de funciones*/
+/* Prototipo de funciones */
 void menu();
 void ingresarEdades();
-void principal();
+void start();
 void showages();
+void edadMayor();
+void edadMenor();
+void promedioEdades();
 
-/*funciones*/
-void menu(){
+/* Funciones */
+void menu() {
     system("cls");
-    cout<<"1. Ingresar edades"<<endl;
-    cout<<"2. Mostrar edades"<<endl;
-    cout<<"3. Mostrar edad mayor"<<endl;
-    cout<<"4. Mostrar edad menor"<<endl;
-    cout<<"5. Mostrar promedio de edades"<<endl;
-    cout<<"6. Salir"<<endl;
+    cout << "1. Ingresar edades" << endl;
+    cout << "2. Mostrar edades" << endl;
+    cout << "3. Mostrar edad mayor" << endl;
+    cout << "4. Mostrar edad menor" << endl;
+    cout << "5. Mostrar promedio de edades" << endl;
+    cout << "6. Salir" << endl;
 }
 
-/*Agregar edades */
-void ingresarEdades(){
+/* Agregar edades */
+void ingresarEdades() {
     system("cls");
-    cout<<"Ingrese la edad: ";
-    cin>>age[post];
+    cout << "Ingrese la edad: ";
+    cin >> age[post];
     post++;
     system("pause");
 }
 
-void start(){
-    int option;
+void start() {
+    int option = 0;
 
-    while (option!=6)
-    {
+    while (option != 6) {
         menu();
-        cout<<"Ingrese una opcion: ";
-        cin>>option;
-        switch (option)
-        {
+        cout << "Ingrese una opcion: ";
+        cin >> option;
+        switch (option) {
         case 1:
             ingresarEdades();
             break;
         case 2:
             showages();
             break;
-        case 6:
-            cout<<"adios....!"<<endl;
+        case 3:
+            edadMayor();
             break;
-
+        case 4:
+            edadMenor();
+            break;
+        case 5:
+            promedioEdades();
+            break;
+        case 6:
+            cout << "Adios....!" << endl;
+            break;
         default:
+            cout << "Opcion no valida, intente nuevamente." << endl;
+            system("pause");
             break;
         }
     }
 }
 
-void showages(){
+void showages() {
     system("cls");
-    cout << "edades: " << endl;
-    for (int i = 0; i < post; i++)
-    {
-        cout<<age[i]<<endl;
+    cout << "Edades: " << endl;
+    for (int i = 0; i < post; i++) {
+        cout << age[i] << endl;
+    }
+    system("pause");
+}
+
+void edadMayor() {
+    system("cls");
+    if (post == 0) {
+        cout << "No hay edades ingresadas." << endl;
+    } else {
+        int mayor = age[0];
+        for (int i = 1; i < post; i++) {
+            if (age[i] > mayor) {
+                mayor = age[i];
+            }
+        }
+        cout << "La edad mayor es: " << mayor << endl;
+    }
+    system("pause");
+}
+
+void edadMenor() {
+    system("cls");
+    if (post == 0) {
+        cout << "No hay edades ingresadas." << endl;
+    } else {
+        int menor = age[0];
+        for (int i = 1; i < post; i++) {
+            if (age[i] < menor) {
+                menor = age[i];
+            }
+        }
+        cout << "La edad menor es: " << menor << endl;
+    }
+    system("pause");
+}
+
+void promedioEdades() {
+    system("cls");
+    if (post == 0) {
+        cout << "No hay edades ingresadas." << endl;
+    } else {
+        int suma = 0;
+        for (int i = 0; i < post; i++) {
+            suma += age[i];
+        }
+        double promedio = static_cast<double>(suma) / post;
+        cout << "El promedio de las edades es: " << promedio << endl;
     }
     system("pause");
 }
--- sesion1705/variable.h
+++ sesion1705/variable.h
@@ -1,4 +1,8 @@
 #define MAX 100
 int age [MAX];
 int post = 0;
-
+int mayor = 0;
+int menor = 0;
+int suna = 0;
+int promedio = 0;
+void start();
