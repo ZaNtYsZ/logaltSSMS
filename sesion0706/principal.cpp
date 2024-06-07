92e8eacdee950d70d4b377800ea605611fcebafe
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-06-07 08:47:47 -0600

    sesion070624

---
+++ sesion0706/principal.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<locale.h>
+#include "variables.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    setlocale(LC_CTYPE, "Spanish");
+    funciones();
+    return 0;
+}
+
---
+++ sesion0706/variables.h
@@ -0,0 +1,8 @@
+#define MAX_REG 100
+
+typedef struct CIUDAD {
+    int id;
+    char nimbre[50];
+    char descripcion[100];
+}
+
